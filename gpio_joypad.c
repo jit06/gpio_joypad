226fd18f7fcf313604425790f9630274ad734788
jit06 <juju@atchoum.local.lan>
2019-09-30 21:13:48 +0200

    some adjustement to upper and lower calues limits. Added 2 differents value for threshold (when to trigger change) and margin for the position zero

--- gpio_joypad.c
+++ gpio_joypad.c
@@ -14,23 +14,24 @@
 #define ADC_SW_1   21  // gpio pin for switch 1 
 #define ADC_SW_2   22  // gpio pin for switch 2
 
-#define ADC_THRESHOLD 40
+#define ADC_THRESHOLD 8
+#define ADC_MID_MARGIN 20
 
 #define	ADC_HAT0X_MAX 600
-#define ADC_HAT0X_MIN 125
-#define ADC_HAT0X_FLA 365
+#define ADC_HAT0X_MIN 120
+#define ADC_HAT0X_FLA 353
 
-#define	ADC_HAT0Y_MAX 630
-#define ADC_HAT0Y_MIN 141
-#define ADC_HAT0Y_FLA 357
+#define	ADC_HAT0Y_MAX 620
+#define ADC_HAT0Y_MIN 120
+#define ADC_HAT0Y_FLA 358
 
-#define	ADC_HAT1X_MAX 635
-#define ADC_HAT1X_MIN 160
-#define ADC_HAT1X_FLA 356
+#define	ADC_HAT1X_MAX 600
+#define ADC_HAT1X_MIN 120
+#define ADC_HAT1X_FLA 345
 
-#define	ADC_HAT1Y_MAX 625
-#define ADC_HAT1Y_MIN 140
-#define ADC_HAT1Y_FLA 369
+#define	ADC_HAT1Y_MAX 650
+#define ADC_HAT1Y_MIN 120
+#define ADC_HAT1Y_FLA 375
 
 #define ANALOG_MAX 32767
 #define ANALOG_MID 0
@@ -42,33 +43,35 @@ struct {
 	int status;    // current status : 1 = released, 0 = pressed
 	int initial;   // initial pin status (should always be 1)
         int activeVal; // value to set when active
+        int clicked;   // track a clicked event to handle special combos
 	int eventType; // EV_KEY or EV_ABS
 } io[] = {
-	{  3	, BTN_DPAD_UP   , 1 , 1 , 1 , EV_KEY  }, // UP
-        {  3	, ABS_HAT0Y     , 1 , 1 , -1, EV_ABS  }, // UP
-	{  0	, BTN_DPAD_DOWN , 1 , 1 , 1 , EV_KEY  }, // DOWN
-        {  0	, ABS_HAT0Y     , 1 , 1 , 1 , EV_ABS  }, // DOWN
-	{ 23	, BTN_DPAD_LEFT , 1 , 1 , 1 , EV_KEY  }, // LEFT
-        { 23	, ABS_HAT0X     , 1 , 1 , -1, EV_ABS  }, // LEFT
-	{  2	, BTN_DPAD_RIGHT, 1 , 1 , 1 , EV_KEY  }, // RIGHT
-        {  2	, ABS_HAT0X     , 1 , 1 , 1 , EV_ABS  }, // RIGHT
-	{ 14	, BTN_X         , 1 , 1 , 1 , EV_KEY  }, // X
-	{ 11	, BTN_A         , 1 , 1 , 1 , EV_KEY  }, // A
-	{  6	, BTN_B         , 1 , 1 , 1 , EV_KEY  }, // B
-	{ 26	, BTN_Y         , 1 , 1 , 1 , EV_KEY  }, // Y
-	{  5	, BTN_TL        , 1 , 1 , 1 , EV_KEY  }, // L1
-	{  7	, BTN_TL2       , 1 , 1 , 1 , EV_KEY  }, // L2
-	{ 12	, BTN_TR        , 1 , 1 , 1 , EV_KEY  }, // R1
-	{  1	, BTN_TR2       , 1 , 1 , 1 , EV_KEY  }, // R2
-	{ 10	, BTN_START     , 1 , 1 , 1 , EV_KEY  }, // START
-	{  4	, BTN_SELECT    , 1 , 1 , 1 , EV_KEY  }, // SELET
-	{ 24	, KEY_VOLUMEUP  , 1 , 1 , 1 , EV_KEY  }, // Vol up
-	{ 27	, KEY_VOLUMEDOWN, 1 , 1 , 1 , EV_KEY  }, // Vol down
-	{ -1	, -1            ,- 1,-1 , 1 , EV_KEY  }  // DO NOT REMOVE
+	{  3	, BTN_DPAD_UP   , 1 , 1 , 1 , 0, EV_KEY  }, // UP
+        {  3	, ABS_HAT0Y     , 1 , 1 , -1, 0, EV_ABS  }, // UP
+	{  0	, BTN_DPAD_DOWN , 1 , 1 , 1 , 0, EV_KEY  }, // DOWN
+        {  0	, ABS_HAT0Y     , 1 , 1 , 1 , 0, EV_ABS  }, // DOWN
+	{ 23	, BTN_DPAD_LEFT , 1 , 1 , 1 , 0, EV_KEY  }, // LEFT
+        { 23	, ABS_HAT0X     , 1 , 1 , -1, 0, EV_ABS  }, // LEFT
+	{  2	, BTN_DPAD_RIGHT, 1 , 1 , 1 , 0, EV_KEY  }, // RIGHT
+        {  2	, ABS_HAT0X     , 1 , 1 , 1 , 0, EV_ABS  }, // RIGHT
+	{ 14	, BTN_X         , 1 , 1 , 1 , 0, EV_KEY  }, // X
+	{ 11	, BTN_A         , 1 , 1 , 1 , 0, EV_KEY  }, // A
+	{  6	, BTN_B         , 1 , 1 , 1 , 0, EV_KEY  }, // B
+	{ 26	, BTN_Y         , 1 , 1 , 1 , 0, EV_KEY  }, // Y
+	{  5	, BTN_TL        , 1 , 1 , 1 , 0, EV_KEY  }, // L1
+	{  7	, BTN_TL2       , 1 , 1 , 1 , 0, EV_KEY  }, // L2
+	{ 12	, BTN_TR        , 1 , 1 , 1 , 0, EV_KEY  }, // R1
+	{  1	, BTN_TR2       , 1 , 1 , 1 , 0, EV_KEY  }, // R2
+	{ 10	, BTN_START     , 1 , 1 , 1 , 0, EV_KEY  }, // START
+	{  4	, BTN_SELECT    , 1 , 1 , 1 , 0, EV_KEY  }, // SELET
+	{ 24	, KEY_VOLUMEUP  , 1 , 1 , 1 , 0, EV_KEY  }, // Vol up
+	{ 27	, KEY_VOLUMEDOWN, 1 , 1 , 1 , 0, EV_KEY  }, // Vol down
+	{ -1	, -1            ,- 1,-1 , 1 , 0, EV_KEY  }  // DO NOT REMOVE
 };
 
 int Running = 1;
 int UinputFd = -1;
+int SoundMuted = 0;
 struct uinput_user_dev UiJoypad;
 struct input_event ev;
 
@@ -103,17 +106,19 @@ void sendEvent(int type, int code, int value) {
         err("sending button event failed");
 }
 
+
 // send converted analog value from adc to uinput
 int sendAnalog(int sw1, int sw2, int *adc, int *lastadc, int evt, int hatlow, int hatmid, int hatmax, int reverse) {
     int state;
     *adc = adcRead(sw1, sw2);
 
-    if (abs(*lastadc - *adc) > ADC_THRESHOLD) {
-        state = translateAnalog(*adc, ADC_THRESHOLD, hatlow, hatmid, hatmax, reverse);
-        sendEvent(EV_ABS, evt, state);
+    if ( (*adc >= 0) && (abs(*lastadc - *adc) > ADC_THRESHOLD) ) {
+//	printf("last / cur %i / %i\n",*lastadc,*adc);
         *lastadc = *adc;
-	printf("analog %i %i: %i\n",sw1,sw2, *adc);
-	printf("transl %i %i: %i\n",sw1,sw2, state);
+        state = translateAnalog(*adc, ADC_MID_MARGIN, hatlow, hatmid, hatmax, reverse);
+        sendEvent(EV_ABS, evt, state);
+//	printf("analog %i %i: %i\n",sw1,sw2, *adc);
+//	printf("transl %i %i: %i\n",sw1,sw2, state);
 	return 1;
     } else {
         return 0;
@@ -135,6 +140,11 @@ int translateAnalog(int value, int threshhold, int hatlow, int hatmid, int hatma
     if (reverse == 1)
         retval = retval * -1;
 
+    if (retval > ANALOG_MAX) retval = ANALOG_MAX;
+    if (retval < ANALOG_MIN) retval = ANALOG_MIN;
+
+//    printf("translate value / final = %i / %i\n", value, retval);
+
     return retval;
 }
 
@@ -149,15 +159,30 @@ int adcRead(int sw1, int sw2) {
 }
 
 
+void handleVolume() {
+    // volup (18) => up volume with amixer
+    if(io[18].clicked == 1) {
+        system("/usr/bin/amixer -c 1 -q set Master 1%+;/usr/bin/amixer -c 1 -q set Speaker 1%+");
+        io[18].clicked = 0;
+    }
+    // voldown (19) => down volume with amixer
+    if(io[19].clicked == 1) {
+        system("/usr/bin/amixer -c 1 -q set Master 1%-;/usr/bin/amixer -c 1 -q set Speaker 1%-");
+        io[19].clicked = 0;
+    }
+}
+
+
 void init(void) {
     int i=0;
 
     // init uinput
     UinputFd = open("/dev/uinput", O_WRONLY | O_NONBLOCK);
     if(UinputFd < 0) {
-        err("failed to open uinput");
+        err("failed to open uinputfor gamepad");
     }
 
+    // gamepad
     if(ioctl(UinputFd, UI_SET_EVBIT, EV_KEY)     <0 ) err("ioctl enable key failed"); // activate key press / realease
     if(ioctl(UinputFd, UI_SET_EVBIT, EV_ABS)     <0 ) err("ioctl enable abs failed"); // activate absolute axis
     if(ioctl(UinputFd, UI_SET_ABSBIT, ABS_X)     <0 ) err("ioctl enable X1 axis failed"); // activate analog X1 axis
@@ -167,8 +192,7 @@ void init(void) {
     if(ioctl(UinputFd, UI_SET_ABSBIT, ABS_HAT0X) <0 ) err("ioctl enable HAT0X axis failed");
     if(ioctl(UinputFd, UI_SET_ABSBIT, ABS_HAT0Y) <0 ) err("ioctl enable HAT0Y axis failed");
 
-
-    // set-up gpio pin & used uinput code
+    // set-up gpio pin & used uinput codefor gamepad
     while( io[i].pin >= 0 ) {
         pinMode (io[i].pin, INPUT) ;          // init used gpio pin as input
         pullUpDnControl(io[i].pin, PUD_UP);   // init used gpio pin as pullup
@@ -230,7 +254,7 @@ int main (void)
     init();
     delay(2000);
     int i, change=0;
-    int adcx1, adcy1, adcx2, adcy2, last_adcx1=0, last_adcy1=0, last_adcx2, last_adcy2;
+    int adcx1=0, adcy1=0, adcx2=0, adcy2=0, last_adcx1=ADC_HAT0X_FLA, last_adcy1=ADC_HAT0Y_FLA, last_adcx2=ADC_HAT1X_FLA, last_adcy2=ADC_HAT1Y_FLA;
 
     while(Running) {
         i=0;
@@ -239,14 +263,15 @@ int main (void)
         while( io[i].pin >= 0 ) {
 
             if(digitalRead(io[i].pin)!=io[i].initial && io[i].status == io[i].initial) {
-                printf("Pressed key=%i (pin %i) value = %i!\n", io[i].key, io[i].pin, io[i].activeVal);
+//                printf("Pressed key=%i (pin %i) value = %i!\n", io[i].key, io[i].pin, io[i].activeVal);
 		sendEvent(io[i].eventType, io[i].key, io[i].activeVal);
                 io[i].status = 0;   // ... mean value 0 for gpio pin
+                io[i].clicked = 1;
                 change=1;
             }
 
             if(digitalRead(io[i].pin)==io[i].initial && io[i].status != io[i].initial) {
-                printf("Released %i (%i)!\n", io[i].key, io[i].pin);
+//                printf("Released %i (%i)!\n", io[i].key, io[i].pin);
                 sendEvent(io[i].eventType, io[i].key, 0);
                 change=1;
                 io[i].status = 1;   // ... mean value 1 for gpio pin
@@ -254,6 +279,8 @@ int main (void)
 
             i++;
         }
+
+        handleVolume();
 
         // handle analog stick
         if(sendAnalog(1,0,&adcx1,&last_adcx1,ABS_X,ADC_HAT0X_MIN, ADC_HAT0X_FLA, ADC_HAT0X_MAX,1))  change = 1;
